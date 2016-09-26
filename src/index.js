3666c261bf484f72d2533b9bd247f1994a2ac68b
gomugom <power4ce@gmail.com>
2016-09-26 12:50:58 +0900

    router 정리

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { browserHistory, Router } from 'react-router';
+import { browserHistory, Router, Route, IndexRoute } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import store, { getState } from './App/store';
@@ -9,13 +9,23 @@ import routes from './App/routes';
 import './scss/normalize.scss';
 import './scss/style.scss';
 
+import App from './App/App';
+import Home from './Home/components/Home';
+import Gomugom from './Gomugom/components/Gomugom';
+import ArusantimoMain from './Arusantimo/components/ArusantimoMain';
+
 const history = syncHistoryWithStore(browserHistory, store);
 
 render((
 	<Provider store={store}>
-		<Router
-			history={history}
-			routes={routes(getState)}
-		/>
+		<Router history={history}>
+			<Route path="/" component={App}>
+				<IndexRoute component={Home} />
+				<Route path="gomugom" component={Gomugom}
+					onEnter={()=>{ console.log('Gomugom page rendered'); }} />
+				<Route path="arusantimo" component={ArusantimoMain}
+					onEnter={()=>{ console.log('welcome Arusantimo page!! enjoy~~'); }} />
+			</Route>
+		</Router>
 	</Provider>
 ), document.getElementById('root'));
